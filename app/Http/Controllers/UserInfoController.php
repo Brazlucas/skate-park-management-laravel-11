02cd5c545011c048893c3ed3829e1e643fe4c83c
Brazlucas <107437261+Brazlucas@users.noreply.github.com>
2025-03-13 20:12:40 -0300

    Revert "feat: new admin implementations and validations. New entity: location"

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -4,42 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 use App\Http\Resources\UserResource;
-use Illuminate\Database\Eloquent\Collection;
 
 class UserInfoController extends Controller
 {
     /**
-     * Get all users' information.
-     */
-    public function index(): Collection
-    {
-        try {
-            $users = User::all();
-            return $users;
-
-        } catch (\Exception $e) {
-            return response()->json([
-                'error' => 'Erro ao obter informações dos usuários',
-                'message' => $e->getMessage(),
-            ], 500);
-        }
-    }
-
-    /**
      * Get the authenticated user's information.
      */
-    public function me(): JsonResponse
+    public function index(): JsonResponse
     {
         try {
             $user = Auth::user();
-
-            if (!$user) {
-                return response()->json([
-                    'error' => 'Usuário não autenticado',
-                ], 401);
-            }
 
             return response()->json([
                 'user' => new UserResource($user),
@@ -47,7 +22,7 @@ class UserInfoController extends Controller
 
         } catch (\Exception $e) {
             return response()->json([
-                'error' => 'Erro ao obter informações do usuário autenticado',
+                'error' => 'Erro ao obter informações do usuário',
                 'message' => $e->getMessage(),
             ], 500);
         }
--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,13 @@ use App\Http\Controllers\SkateParkController;
 use App\Http\Controllers\RentalController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserInfoController;
-use App\Http\Controllers\LocationsController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [RegisteredUserController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/users', [UserInfoController::class, 'index']); // Todos os usuários
-    Route::get('/me', [UserInfoController::class, 'me']); // Usuário autenticado
+    Route::apiResource('user-info', UserInfoController::class);
     Route::apiResource('skate-parks', SkateParkController::class);
-    Route::apiResource('locations', LocationsController::class);
     Route::apiResource('rentals', RentalController::class)->shallow();
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh-token', [AuthController::class, 'refreshToken']);
